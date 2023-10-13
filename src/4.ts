8a38c660446e70bd8e7095b0bf9546a8afb8ee2d
opoberezh <opoberezh.2015@gmail.com>
2023-10-13 10:32:17 +0200

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,51 @@
+// const key = new Key();
+
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
+
+
+// export {};
+
+class Key {
+    private signature: number = Math.random();
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+   constructor (private key: Key) {}
+    getKey(): Key {
+    return this.key
+   }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    private tenants: Person[] = [];
+
+    constructor(protected key: Key) {};
+
+    comeIn (person: Person): void{
+        if (this.door){
+            this.tenants.push(person)
+        }
+    }
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if(this.key.getSignature() === key.getSignature()){
+            this.door = true;
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +54,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
